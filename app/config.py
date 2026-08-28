f7032d537afeb63b7521ddac070e37e55f007654
michaelkellerpercussion-creator <michaelkellerpercussion@gmail.com>
2026-08-27 19:32:10 -0700

    prepped for deployment

--- app/config.py
+++ app/config.py
@@ -19,3 +19,8 @@ class TestingConfig:
 class ProductionConfig:
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///mechanic_shop.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+class ProductionConfig:
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI)
+    CACHE_TYPE = "SimpleCache"
--- flask_app.py
+++ flask_app.py
@@ -1,9 +1,7 @@
 from app import create_app, db
 
-app = create_app()
+app = create_app('ProductionConfig')
 
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-
-    app.run(debug=True)
