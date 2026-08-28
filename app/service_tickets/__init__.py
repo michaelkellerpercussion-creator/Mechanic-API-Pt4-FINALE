cdd1ee256fa5430d8bb8117ca2b190d33e135a2e
michaelkellerpercussion-creator <michaelkellerpercussion@gmail.com>
2026-08-28 12:37:07 -0700

    Fix Swagger host domain and enable CORS

--- app/service_tickets/__init__.py
+++ app/service_tickets/__init__.py
@@ -1,5 +1,14 @@
 from flask import Blueprint
+from app.service_tickets import routes
+from flask import Flask
+from flask_cors import CORS
+from flask_swagger_ui import get_swaggerui_blueprint
+
+def create_app(config_class="config.DevelopmentConfig"):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
+
+CORS(app)
 
 service_tickets_bp = Blueprint('service_tickets_bp', __name__)
 
-from app.service_tickets import routes
